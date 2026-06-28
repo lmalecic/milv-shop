eb155d83884d0b0acfde5fa3aa85fea5c4660615
Luka Malečić <luka.malecic@gmail.com>
2026-06-28 21:32:38 +0200

    Fix Admin Tank Roles itemClickPath.

--- src/test/java/com/lmalecic/milvshop/MilvShopApplicationTests.java
+++ src/test/java/com/lmalecic/milvshop/MilvShopApplicationTests.java
@@ -3,15 +3,7 @@ package com.lmalecic.milvshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:h2:mem:milv-shop-test",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "app.admin.username=admin",
-        "app.admin.password=test-admin-password",
-        "jwt.secret=MDEyMzQ1Njc4OWFiY2RlZjAxMjM0NTY3ODlhYmNkZWY=",
-        "jwt.expiration=900000"
-})
+@SpringBootTest
 class MilvShopApplicationTests {
 
     @Test
