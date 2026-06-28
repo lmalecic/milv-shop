1b8ada990d69626076e41781897f493d5c1595ee
Luka Malečić <luka.malecic@gmail.com>
2026-06-28 21:21:01 +0200

    Fix SonarQube reports in DataInitializer

--- src/main/java/com/lmalecic/milvshop/config/DataInitializer.java
+++ src/main/java/com/lmalecic/milvshop/config/DataInitializer.java
@@ -23,6 +23,11 @@ public class DataInitializer implements CommandLineRunner {
     private final TankRepository tankRepository;
     private final AdminProperties adminProperties;
 
+    private static final String ROLE_ADMIN = "ROLE_ADMIN";
+    private static final String GERMAN_REICH = "German Reich";
+    private static final String HEAVY_TANK = "Heavy tank";
+    private static final String MEDIUM_TANK = "Medium tank";
+
     @Override
     public void run(String... args) throws Exception {
         this.initUserRoles();
@@ -33,7 +38,7 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     private void initUserRoles() {
-        List<String> toCheck = List.of("ROLE_ADMIN", "ROLE_USER");
+        List<String> toCheck = List.of(ROLE_ADMIN, "ROLE_USER");
         Set<String> existing = new HashSet<>(this.userRoleRepository.findAllNames());
         this.userRoleRepository.saveAll(toCheck.stream()
                 .filter(roleName -> !existing.contains(roleName))
@@ -54,9 +59,9 @@ public class DataInitializer implements CommandLineRunner {
             this.userRepository.save(User.builder()
                     .username(this.adminProperties.getUsername())
                     .pwdHash(this.passwordEncoder.encode(this.adminProperties.getPassword()))
-                    .roles(List.of(this.userRoleRepository.findByName("ROLE_ADMIN")
+                    .roles(List.of(this.userRoleRepository.findByName(ROLE_ADMIN)
                                             .orElse(UserRole.builder()
-                                                            .name("ROLE_ADMIN")
+                                                            .name(ROLE_ADMIN)
                                                             .build())))
                     .build());
         }
@@ -66,7 +71,7 @@ public class DataInitializer implements CommandLineRunner {
         record NationSeed(String name, String imgPath) {}
         List<NationSeed> toCheck = List.of(
                 new NationSeed("USA", "/img/nation/usa.svg"),
-                new NationSeed("German Reich", "/img/nation/german_reich.svg"),
+                new NationSeed(GERMAN_REICH, "/img/nation/german_reich.svg"),
                 new NationSeed("USSR", "/img/nation/ussr.svg"),
                 new NationSeed("Great Britain", "/img/nation/uk.svg")
         );
@@ -85,8 +90,8 @@ public class DataInitializer implements CommandLineRunner {
         record TankRoleSeed(String name, String imgPath) {}
         List<TankRoleSeed> toCheck = List.of(
                 new TankRoleSeed("Light tank", "/img/tankrole/light_tank.svg"),
-                new TankRoleSeed("Medium tank", "/img/tankrole/medium_tank.svg"),
-                new TankRoleSeed("Heavy tank", "/img/tankrole/heavy_tank.svg")
+                new TankRoleSeed(MEDIUM_TANK, "/img/tankrole/medium_tank.svg"),
+                new TankRoleSeed(HEAVY_TANK, "/img/tankrole/heavy_tank.svg")
         );
         Set<String> existing = new HashSet<>(this.tankRoleRepository.findAllNames());
         this.tankRoleRepository.saveAll(toCheck.stream()
@@ -108,8 +113,8 @@ public class DataInitializer implements CommandLineRunner {
                 .collect(Collectors.toMap(TankRole::getName, role -> role));
 
         List<TankSeed> toCheck = List.of(
-                new TankSeed(nations.get("German Reich"),
-                        tankRoles.get("Heavy tank"),
+                new TankSeed(nations.get(GERMAN_REICH),
+                        tankRoles.get(HEAVY_TANK),
                         "Tiger II",
                         "/img/tank/tiger2.png",
                         "The Panzerkampfwagen Tiger Ausf. B (Sd.Kfz. Index: Sd.Kfz. 182), also known as the Tiger II or informally the Königstiger (lit. \"King Tiger\"), was a German heavy tank developed in 1943 by Henschel to serve as a replacement for the Tiger I.",
@@ -118,8 +123,8 @@ public class DataInitializer implements CommandLineRunner {
                         185,
                         38,
                         5),
-                new TankSeed(nations.get("German Reich"),
-                        tankRoles.get("Heavy tank"),
+                new TankSeed(nations.get(GERMAN_REICH),
+                        tankRoles.get(HEAVY_TANK),
                         "Tiger H1",
                         "/img/tank/tigerh1.png",
                         "The Panzerkampfwagen VI Ausführung H1 (Tiger H1) is the first (early-production) variant of the Tiger I heavy tank family, designed and built by Henschel and used by the German Army during World War II.",
@@ -128,8 +133,8 @@ public class DataInitializer implements CommandLineRunner {
                         100,
                         45,
                         5),
-                new TankSeed(nations.get("German Reich"),
-                        tankRoles.get("Medium tank"),
+                new TankSeed(nations.get(GERMAN_REICH),
+                        tankRoles.get(MEDIUM_TANK),
                         "Panther A",
                         "/img/tank/panther_a.png",
                         "The Panzerkampfwagen V Ausführung A (Panther A) (Sd.Kfz. Index: Sd.Kfz. 171) is the second production variant of the iconic Panzerkampfwagen V Panther medium tank family.",
@@ -139,7 +144,7 @@ public class DataInitializer implements CommandLineRunner {
                         46,
                         5),
                 new TankSeed(nations.get("USA"),
-                        tankRoles.get("Heavy tank"),
+                        tankRoles.get(HEAVY_TANK),
                         "M4A3E2",
                         "/img/tank/sherman_jumbo.png",
                         "The M4A3E2 Sherman - Assault Tank is an armoured modification of the M4A3, which is the fourth variant of the early-generation Medium Tank M4 (Sherman) family.",
@@ -149,7 +154,7 @@ public class DataInitializer implements CommandLineRunner {
                         35,
                         5),
                 new TankSeed(nations.get("Great Britain"),
-                        tankRoles.get("Medium tank"),
+                        tankRoles.get(MEDIUM_TANK),
                         "Centurion Mk 3",
                         "/img/tank/cent_mk3.png",
                         "The Centurion Mk 3 was the third variant of the Centurion medium tank family. The 84 mm Ordnance QF 20-pounder tank gun was installed in this variant, which provided significantly superior accuracy thanks to a newly developed two-plane fully automatic stabilization system (modified from the Centurion Mk 2).",
